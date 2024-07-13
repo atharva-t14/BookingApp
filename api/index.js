8ae15232e8f7cc75a8bbaff9dc77002d364b47cf
Atharva Tamhankar <atharvatamhankar11@gmail.com>
2024-07-14 00:07:37 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import roomsRoute from "./routes/rooms.js"
+import hotelsRoute from "./routes/hotels.js"
+import usersRoute from "./routes/users.js"
+const app = express();
+
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!")
+});
+mongoose.connection.on("connected", () => {
+    console.log("mongoDB connected!")
+});
+
+// middlewares
+app.use(express.json())
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", usersRoute)
+app.use("/api/hotels", hotelsRoute)
+app.use("/api/rooms", roomsRoute)
+
+// app.use((req, res, next) => {
+//     console.log("Hi im a middleware")
+// })
+
+// error handling middleware
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500
+    const errorMessage = err.message || "Something went wrong!"
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
+
+app.listen(3300, () => {
+    connect()
+    console.log("Connected to backend.");
+});
